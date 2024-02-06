3abb7324810d983613fedcdc0b7fca83378ad6d7
Joswin Prince <87509443+joswinprince@users.noreply.github.com>
2024-02-06 10:15:34 +0530

    Create CelsiusToFahrenheit.java

---
+++ basic/CelsiusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        float celsius = 37;
+        float fahrenheit = (celsius * 9/5) + 32;
+        System.out.println("Temperature in Fahrenheit is: " + fahrenheit);
+    }
+}
